0dad51363418f1aea414822a6329412f332ec59d
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-06-06 18:49:16 +0000

    fixed borders (thank you Ayat and Isaac)

--- script.js
+++ script.js
@@ -4,82 +4,81 @@ const teamMembers = [
         name: 'Darth Vader',
         species: 'Human',
         saberColor: 'red',
-        description:'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
-        img:'vader.png'
+        description: 'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
+        img: 'vader.png'
     },
     {
         name: 'Darth Vader',
         species: 'Human',
         saberColor: 'red',
-        description:'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
-        img:'vader.png'
+        description: 'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
+        img: 'vader.png'
     },
     {
         name: 'Darth Vader',
         species: 'Human',
         saberColor: 'red',
-        description:'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
-        img:'vader.png'
+        description: 'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
+        img: 'vader.png'
     },
     {
         name: 'Darth Vader',
         species: 'Human',
         saberColor: 'red',
-        description:'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
-        img:'vader.png'
+        description: 'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
+        img: 'vader.png'
     },
     {
         name: 'Darth Vader',
         species: 'Human',
         saberColor: 'red',
-        description:'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
-        img:'vader.png'
+        description: 'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
+        img: 'vader.png'
     },
     {
         name: 'Darth Vader',
         species: 'Human',
         saberColor: 'red',
-        description:'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
-        img:'vader.png'
+        description: 'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.',
+        img: 'vader.png'
     },
 
-    
-    ]
-    
-    function generateTeamCards(){
-    
+
+]
+
+function generateTeamCards() {
+
     const teamCardsContainer = document.getElementById('teamCards')
-    
+
     teamMembers.forEach(member => {
         const card = document.createElement('div')
         card.classList.add('col-md-3', 'm-3')
-    
-       
-    
-    
-    let backgroundColor
-    
-    switch(member.saberColor.toLowerCase()){
-        case 'red':
-            backgroundColor = "red"
-            break
+
+
+
+
+        let backgroundColor = ''
+
+        switch (member.saberColor.toLowerCase()) {
+            case 'red':
+                backgroundColor = "red"
+                break
             case 'blue':
-            backgroundColor = "blue"
-            break
+                backgroundColor = "blue"
+                break
             case 'green':
-            backgroundColor = "green"
-            break
+                backgroundColor = "green"
+                break
             case 'purple':
-            backgroundColor = "purple"
-            break
+                backgroundColor = "purple"
+                break
             case '':
-            backgroundColor = "gray"
-            break
-    }
-    card.style.backgroundColor = backgroundColor
-    
-    
-        card.innerHTML= `
+                backgroundColor = "gray"
+                break
+        }
+
+
+        card.innerHTML = `
         <div class="card m-5">
             <div class = "card-header text-center">
             <img src="${member.img}" class="img-fluid card-img">
@@ -93,8 +92,13 @@ const teamMembers = [
         </div>
       
         `
-        
+        for (let i = 1; i < card.children.length; i++) {
+            card.children[i].style.backgroundColor = backgroundColor;
+        }
+
+
+
         teamCardsContainer.appendChild(card)
     });
-    }
-    window.onload = generateTeamCards()
+}
+window.onload = generateTeamCards()
